7783d6d9c665b1467f822e0b0f06804f91be2dc1
David Wickström <davidwickstrom@gmail.com>
2015-03-18 15:19:35 +0100

    Fix typo

--- src/app/code/community/KL/Stock/Model/Cron.php
+++ src/app/code/community/KL/Stock/Model/Cron.php
@@ -4,6 +4,6 @@ class KL_Stock_Model_Cron
 {
     public function fire()
     {
-        Mage::dispathEvent('time_to_fix_stock_status_was_reached');
+        Mage::dispatchEvent('time_to_fix_stock_status_was_reached');
     }
 }
